54b9337c44ae963bbc06e2e9f8bf849f0a186f3a
SaraGhorbani <sara.qorbani73@gmail.com>
2022-11-03 17:41:17 +0330

    add forground service locaiton in android native

--- android/app/src/main/kotlin/com/example/location_service_best/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/location_service_best/MainActivity.kt
@@ -1,44 +1,15 @@
-/*
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.location_service_best
 
+import SharedPreferenceUtil
 import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.content.ServiceConnection
-import android.content.SharedPreferences
+import android.content.*
 import android.content.pm.PackageManager
 import android.location.Location
-import android.net.Uri
-import android.os.Bundle
 import android.os.IBinder
-import android.provider.Settings
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.example.location_service_best.BuildConfig
-import com.example.location_service_best.R
-import com.google.android.material.snackbar.Snackbar
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
@@ -47,64 +18,13 @@ import toText
 
 private const val TAG = "MainActivity"
 private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
-
-/**
- *  This app allows a user to receive location updates without the background permission even when
- *  the app isn't in focus. This is the preferred approach for Android.
- *
- *  It does this by creating a foreground service (tied to a Notification) when the
- *  user navigates away from the app. Because of this, it only needs foreground or "while in use"
- *  location permissions. That is, there is no need to ask for location in the background (which
- *  requires additional permissions in the manifest).
- *
- *  Note: Users have the following options in Android 11+ regarding location:
- *
- *  * Allow all the time
- *  * Allow while app is in use, i.e., while app is in foreground (new in Android 10)
- *  * Allow one time use (new in Android 11)
- *  * Not allow location at all
- *
- * It is generally recommended you only request "while in use" location permissions (location only
- * needed in the foreground), e.g., fine and coarse. If your app has an approved use case for
- * using location in the background, request that permission in context and separately from
- * fine/coarse location requests. In addition, if the user denies the request or only allows
- * "while-in-use", handle it gracefully. To see an example of background location, please review
- * {@link https://github.com/android/location-samples/tree/master/LocationUpdatesBackgroundKotlin}.
- *
- * Android 10 and higher also now requires developers to specify foreground service type in the
- * manifest (in this case, "location").
- *
- * For the feature that requires location in the foreground, this sample uses a long-running bound
- * and started service for location updates. The service is aware of foreground status of this
- * activity, which is the only bound client in this sample.
- *
- * While getting location in the foreground, if the activity ceases to be in the foreground (user
- * navigates away from the app), the service promotes itself to a foreground service and continues
- * receiving location updates.
- *
- * When the activity comes back to the foreground, the foreground service stops, and the
- * notification associated with that foreground service is removed.
- *
- * While the foreground service notification is displayed, the user has the option to launch the
- * activity from the notification. The user can also remove location updates directly from the
- * notification. This dismisses the notification and stops the service.
- */
 class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     private var foregroundOnlyLocationServiceBound = false
-
-    // Provides location updates for while-in-use feature.
     private var foregroundOnlyLocationService: ForegroundOnlyLocationService? = null
-
-    // Listens for location broadcasts from ForegroundOnlyLocationService.
     private lateinit var foregroundOnlyBroadcastReceiver: ForegroundOnlyBroadcastReceiver
-
     private lateinit var sharedPreferences: SharedPreferences
 
-//    private lateinit var foregroundOnlyLocationButton: Button
 
-//    private lateinit var outputTextView: TextView
-
-    // Monitors connection to the while-in-use service.
     private val foregroundOnlyServiceConnection = object : ServiceConnection {
 
         override fun onServiceConnected(name: ComponentName, service: IBinder) {
@@ -119,36 +39,6 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
         }
     }
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-////        setContentView(R.layout.activity_main)
-//
-//        foregroundOnlyBroadcastReceiver = ForegroundOnlyBroadcastReceiver()
-//
-//        sharedPreferences =
-//            getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-//
-////        foregroundOnlyLocationButton = findViewById(R.id.foreground_only_location_button)
-////        outputTextView = findViewById(R.id.output_text_view)
-//
-//        foregroundOnlyLocationButton.setOnClickListener {
-//            val enabled = sharedPreferences.getBoolean(
-//                SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
-//
-//            if (enabled) {
-//                foregroundOnlyLocationService?.unsubscribeToLocationUpdates()
-//            } else {
-//                // TODO: Step 1.0, Review Permissions: Checks and requests if needed.
-//                if (foregroundPermissionApproved()) {
-//                    foregroundOnlyLocationService?.subscribeToLocationUpdates()
-//                        ?: Log.d(TAG, "Service Not Bound")
-//                } else {
-//                    requestForegroundPermissions()
-//                }
-//            }
-//        }
-//    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -158,9 +48,6 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
         sharedPreferences =
             getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
 
-//        foregroundOnlyLocationButton.setOnClickListener {
-//            val enabled = sharedPreferences.getBoolean(
-//                SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
 
             MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.example.location_service_best")
                 .setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
@@ -180,26 +67,11 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
                     }
                 }
 
-//            if (enabled) {
-//                foregroundOnlyLocationService?.unsubscribeToLocationUpdates()
-//            } else {
-//                // TODO: Step 1.0, Review Permissions: Checks and requests if needed.
-//                if (foregroundPermissionApproved()) {
-//                    foregroundOnlyLocationService?.subscribeToLocationUpdates()
-//                        ?: Log.d(TAG, "Service Not Bound")
-//                } else {
-//                    requestForegroundPermissions()
-//                }
-//            }
-//        }
     }
 
     override fun onStart() {
         super.onStart()
 
-        updateButtonState(
-            sharedPreferences.getBoolean(SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
-        )
         sharedPreferences.registerOnSharedPreferenceChangeListener(this)
 
         val serviceIntent = Intent(this, ForegroundOnlyLocationService::class.java)
@@ -233,15 +105,10 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        // Updates button states if new while in use location is added to SharedPreferences.
         if (key == SharedPreferenceUtil.KEY_FOREGROUND_ENABLED) {
-            updateButtonState(sharedPreferences.getBoolean(
-                SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
-            )
         }
     }
 
-    // TODO: Step 1.0, Review Permissions: Method checks if permissions approved.
     private fun foregroundPermissionApproved(): Boolean {
         return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
             this,
@@ -253,8 +120,6 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
     private fun requestForegroundPermissions() {
         val provideRationale = foregroundPermissionApproved()
 
-        // If the user denied a previous request, but didn't check "Don't ask again", provide
-        // additional rationale.
         if (provideRationale) {
             Log.i(TAG, "ok: ")
 //            Snackbar.make(
@@ -281,7 +146,6 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
         }
     }
 
-    // TODO: Step 1.0, Review Permissions: Handles permission result.
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
@@ -293,15 +157,13 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
         when (requestCode) {
             REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when {
                 grantResults.isEmpty() ->
-                    // If user interaction was interrupted, the permission request
-                    // is cancelled and you receive empty arrays.
                     Log.d(TAG, "User interaction was cancelled.")
-                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
-                    // Permission was granted.
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->{
+                    Log.i(TAG, "PERMISSION_GRANTED: ")
                     foregroundOnlyLocationService?.subscribeToLocationUpdates()
-                else -> {
-                    // Permission denied.
-                    updateButtonState(false)
+                    Log.i(TAG, "subscribeToLocationUpdates!!!!!!!!!!!!!!!!!!!!!!!: ")
+                }else -> {
+//                    updateButtonState(false)
                     Log.i(TAG, "permission_denied_explanation: ")
 //                    Snackbar.make(
 //                        findViewById(R.id.activity_main),
@@ -327,23 +189,10 @@ class MainActivity : FlutterActivity(), SharedPreferences.OnSharedPreferenceChan
         }
     }
 
-    private fun updateButtonState(trackingLocation: Boolean) {
-        if (trackingLocation) {
-//            foregroundOnlyLocationButton.text = getString(R.string.stop_location_updates_button_text)
-        } else {
-//            foregroundOnlyLocationButton.text = getString(R.string.start_location_updates_button_text)
-        }
-    }
-
     private fun logResultsToScreen(output: String) {
         Log.i(TAG, "location-----------: ${output} ")
-//        val outputWithPreviousLogs = "$output\n${outputTextView.text}"
-//        outputTextView.text = outputWithPreviousLogs
     }
 
-    /**
-     * Receiver for location broadcasts from [ForegroundOnlyLocationService].
-     */
     private inner class ForegroundOnlyBroadcastReceiver : BroadcastReceiver() {
 
         override fun onReceive(context: Context, intent: Intent) {
--- android/app/src/main/kotlin/com/example/location_service_best/Utils.kt
+++ android/app/src/main/kotlin/com/example/location_service_best/Utils.kt
@@ -4,9 +4,6 @@ import android.location.Location
 import androidx.core.content.edit
 import com.example.location_service_best.R
 
-/**
- * Returns the `location` object as a human readable string.
- */
 fun Location?.toText(): String {
     return if (this != null) {
         "($latitude, $longitude)"
@@ -15,27 +12,15 @@ fun Location?.toText(): String {
     }
 }
 
-/**
- * Provides access to SharedPreferences for location to Activities and Services.
- */
 internal object SharedPreferenceUtil {
 
     const val KEY_FOREGROUND_ENABLED = "tracking_foreground_location"
 
-    /**
-     * Returns true if requesting location updates, otherwise returns false.
-     *
-     * @param context The [Context].
-     */
     fun getLocationTrackingPref(context: Context): Boolean =
         context.getSharedPreferences(
          "com.example.android.while_in_use_location.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
             .getBoolean(KEY_FOREGROUND_ENABLED, false)
 
-    /**
-     * Stores the location updates state in SharedPreferences.
-     * @param requestingLocationUpdates The location updates state.
-     */
     fun saveLocationTrackingPref(context: Context, requestingLocationUpdates: Boolean) =
         context.getSharedPreferences(
             context.getString(R.string.preference_file_key),
